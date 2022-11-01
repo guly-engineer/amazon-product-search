ef38c083e54740f876a6bdb1c64b0bb73b9968ec
gbahar <gulbahar.tokus@gmail.com>
2022-11-01 22:57:01 +0000

    update maven.yml

--- src/test/java/com/info/workcast/AmazonProductSearch.java
+++ src/test/java/com/info/workcast/AmazonProductSearch.java
@@ -30,6 +30,7 @@ public class AmazonProductSearch {
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--headless");
+        options.addArguments("--remote-debugging-port=9222");
 
 //        String osName = System.getProperty("os.name").toLowerCase();
 //
